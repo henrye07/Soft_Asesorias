c78431e11e1468dd00d8334ee4aa2fae88a90f79
henrye07 <henryeraso7g@gmail.com>
2022-05-03 04:12:07 -0500

    capo contento

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,9 @@
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+import "primeflex/primeflex.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
